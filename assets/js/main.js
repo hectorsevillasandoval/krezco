63395b7d7bbfa3246e0f823d2222286aeebc1865
hsevilla <sevillasandoval.hector@gmail.com>
2021-01-14 22:56:28 -0600

    main slider has been added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
 //customSelect('select');
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        loop: true,
+        navigation: true,
+        navigationText: [
+            "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
+            "<i class='fa fa-chevron-right'></i>"
+        ],
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: true,
+        slideSpeed: 1000,
+        singleItem: true,
+        transitionStyle: "fadeUp",
+        autoPlay: true
+    });
+    // Slick
+    $('.hero__js').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplayspeed: 3000
+    });
+});
