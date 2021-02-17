1c68377837cfd4e40bc8922a3cadb393c073c55c
renevillanueva1307 <rvillanueva@consultwebs-email.com>
2021-02-17 09:37:31 -0600

    pagina empresa.html y centro.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,20 @@ $(window).load(function() {
         transitionStyle: "fadeUp",
         autoPlay: true
     });
+    //
+    $("#testimonial-slider-2").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        slideSpeed:1000,
+        singleItem:true,
+        transitionStyle:"goDown",
+        navigationText:["",""],
+        autoPlay:false
+    });
     // Slick
     $('.hero__js').slick({
         dots: true,
