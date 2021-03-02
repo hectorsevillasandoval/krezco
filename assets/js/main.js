fe6ad1704288c920cd26481332fff7c4bd86eb02
hsevilla <sevillasandoval.hector@gmail.com>
2021-03-01 21:38:42 -0600

    Adding Popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,17 +19,17 @@ $(window).load(function() {
     });
     //
     $("#testimonial-slider-2").owlCarousel({
-        items:1,
-        itemsDesktop:[1000,2],
-        itemsDesktopSmall:[979,1],
-        itemsTablet:[768,1],
-        pagination:false,
-        navigation:true,
-        slideSpeed:1000,
-        singleItem:true,
-        transitionStyle:"goDown",
-        navigationText:["",""],
-        autoPlay:false
+        items: 1,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: false,
+        navigation: true,
+        slideSpeed: 1000,
+        singleItem: true,
+        transitionStyle: "goDown",
+        navigationText: ["", ""],
+        autoPlay: false
     });
     // Slick
     $('.hero__js').slick({
@@ -70,3 +70,14 @@ window.addEventListener('click', ev => {
         ev.preventDefault();
     }
 });
+
+// Invoke Modals
+let modal = document.getElementById('kr-modal'),
+    callingBtn = '',
+    modalTitle = '';
+
+modal.addEventListener('shown.bs.modal', ev => {
+    callingBtn = ev.relatedTarget;
+    modalTitle = callingBtn.dataset.title;
+    modal.querySelector('.modal-title').innerHTML = modalTitle;
+})
