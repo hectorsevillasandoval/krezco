4d03119cbc213b7edce978fc429b8082707446a2
hsevilla <sevillasandoval.hector@gmail.com>
2021-06-01 00:41:09 -0600

    adding formacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,41 @@ $(window).load(function() {
         navigationText: ["", ""],
         autoPlay: false
     });
+    $('.carousel__js').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style="display: block;"><i class="fas fa-arrow-right"></i></button>',
+        prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="display: block;"><i class="fas fa-arrow-left"></i></button>',
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            }
+            
+        ]
+        
+    });
     // Slick
     $('.hero__js').slick({
         dots: true,
